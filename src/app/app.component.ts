698539511f3048bf86f7f378a792b26914c63935
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-08 14:54:16 -0700

    All done: User can create and delete a todo, as well as have a form validation to make sure the user inserts a value before submitting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,21 @@ export class AppComponent {
   todoArray = [];
 
   /* Create method to add a to-do */
-  addTodo(todo) {
-    this.todoArray.push(todo); /* Push added todo onto the todo "list" */
+  addTodo(todo: any) {
+    todo !== "" ? this.todoArray.push(todo) : alert("Please fill in the to-do field");     
+    console.log("todo: " + todo);
     console.log(this.todoArray);
   }
 
-  deleteTodo(todo) {
+  deleteTodo(todo: any) {
     for (let i = 0; i < this.todoArray.length; i++) { /* Loop through the todoArray O(n) and if the id's match up, delete it */
       if (todo === this.todoArray[i]) { /* Identify if the todo is the one I want to delete */
         this.todoArray.splice(i, 1);
       }      
     }
-
   }
+
+  // todoSubmit(value: any) {
+  //   value !== "" ? this.todoArray.push(value.todo) : alert("Please fill in the To-do field");
+  // }
 }
