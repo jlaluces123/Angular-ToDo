0509cdc6cc2b68385c57e239010599e4b56931de
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-08 14:32:38 -0700

    Method + State: Created the addTodo function to pass to our todoArray state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  /* Initialize a To-Do Array */
+  todoArray = [];
+
+  /* Create method to add a to-do */
+  addTodo(todo) {
+    this.todoArray.push(todo); /* Push added todo onto the todo "list" */
+    console.log(this.todoArray);
+  }
 }
